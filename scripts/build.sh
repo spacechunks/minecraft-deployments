5c1ef0b1f055dd89212e323733a25cd70a361215
Fllip <39161142+FllipEis@users.noreply.github.com>
2024-09-09 18:43:49 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ set -e
 function build() {
   version=$(cat config.pkrvars.json | jq -r .version)
   name=$(cat config.pkrvars.json | jq -r .name)
-  tag=ghcr.io/spacechunks/$name:$version
+  tag=ghcr.io/spacechunks/blueprints/$name:$version
   exists=$(docker manifest inspect $tag > /dev/null ; echo $?)
 
   if [ $exists == 0 ]; then
@@ -16,6 +16,8 @@ function build() {
 }
 
 docker login $BUILD_OCI_REG_SERVER -u $BUILD_OCI_REG_USER -p $BUILD_OCI_REG_PASS
+
+cd blueprints
 
 for dir in */ ; do
   mode=$(basename $dir)
